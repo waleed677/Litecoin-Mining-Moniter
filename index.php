9dada83dbed62c003f15ebe4cffd10574820a7d2
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-08-01 00:45:47 +0500

    Updated site title

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $data = json_decode($data, true);
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-  <title>Litcoing Mining Statistic</title>
+  <title>Litecoin Mining Statistic</title>
 </head>
 
 <body>
